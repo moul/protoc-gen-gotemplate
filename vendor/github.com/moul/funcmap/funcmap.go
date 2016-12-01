19a3db3d0875275c33b4d7b70cf7e6e5f0bc9ae3
Manfred Touron <m@42.am>
2016-12-01 10:08:51 +0100

    bump funcmap@1.1.1

--- vendor/github.com/moul/funcmap/funcmap.go
+++ vendor/github.com/moul/funcmap/funcmap.go
@@ -2,6 +2,8 @@ package funcmap
 
 import (
 	"encoding/json"
+	"fmt"
+	"strconv"
 	"strings"
 	"text/template"
 )
@@ -21,6 +23,23 @@ var FuncMap = template.FuncMap{
 	"split": strings.Split,
 	"join":  strings.Join,
 	"title": strings.Title,
+	"unexport": func(input string) string {
+		return fmt.Sprintf("%s%s", strings.ToLower(input[0:1]), input[1:])
+	},
 	"lower": strings.ToLower,
 	"upper": strings.ToUpper,
+	"rev": func(v interface{}) string {
+		runes := []rune(v.(string))
+		for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+			runes[i], runes[j] = runes[j], runes[i]
+		}
+		return string(runes)
+	},
+	"int": func(v interface{}) string {
+		a, err := strconv.Atoi(v.(string))
+		if err != nil {
+			return fmt.Sprintf("%v", v)
+		}
+		return fmt.Sprintf("%d", a)
+	},
 }
