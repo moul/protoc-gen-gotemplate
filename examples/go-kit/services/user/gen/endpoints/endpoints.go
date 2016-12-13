d252b157c95c7291d7ffe837175c749d46d9fa2d
Manfred Touron <m@42.am>
2016-12-13 17:19:04 +0100

    Add user-service generated-code

---
+++ examples/go-kit/services/user/gen/endpoints/endpoints.go
@@ -0,0 +1,63 @@
+package user_endpoints
+
+import (
+	"fmt"
+	"github.com/go-kit/kit/endpoint"
+	pb "github.com/moul/protoc-gen-gotemplate/examples/go-kit/services/user/gen/pb"
+	context "golang.org/x/net/context"
+)
+
+var _ = fmt.Errorf
+
+type Endpoints struct {
+	CreateUserEndpoint endpoint.Endpoint
+
+	GetUserEndpoint endpoint.Endpoint
+}
+
+func (e *Endpoints) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	out, err := e.CreateUserEndpoint(ctx, in)
+	if err != nil {
+		return &pb.CreateUserResponse{ErrMsg: err.Error()}, err
+	}
+	return out.(*pb.CreateUserResponse), err
+}
+
+func (e *Endpoints) GetUser(ctx context.Context, in *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	out, err := e.GetUserEndpoint(ctx, in)
+	if err != nil {
+		return &pb.GetUserResponse{ErrMsg: err.Error()}, err
+	}
+	return out.(*pb.GetUserResponse), err
+}
+
+func MakeCreateUserEndpoint(svc pb.UserServiceServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(*pb.CreateUserRequest)
+		rep, err := svc.CreateUser(ctx, req)
+		if err != nil {
+			return &pb.CreateUserResponse{ErrMsg: err.Error()}, err
+		}
+		return rep, nil
+	}
+}
+
+func MakeGetUserEndpoint(svc pb.UserServiceServer) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(*pb.GetUserRequest)
+		rep, err := svc.GetUser(ctx, req)
+		if err != nil {
+			return &pb.GetUserResponse{ErrMsg: err.Error()}, err
+		}
+		return rep, nil
+	}
+}
+
+func MakeEndpoints(svc pb.UserServiceServer) Endpoints {
+	return Endpoints{
+
+		CreateUserEndpoint: MakeCreateUserEndpoint(svc),
+
+		GetUserEndpoint: MakeGetUserEndpoint(svc),
+	}
+}
---
+++ examples/go-kit/services/user/gen/transports/grpc/grpc.go
@@ -0,0 +1,75 @@
+package user_grpctransport
+
+import (
+	"fmt"
+
+	grpctransport "github.com/go-kit/kit/transport/grpc"
+	endpoint "github.com/moul/protoc-gen-gotemplate/examples/go-kit/services/user/gen/endpoints"
+	pb "github.com/moul/protoc-gen-gotemplate/examples/go-kit/services/user/gen/pb"
+	context "golang.org/x/net/context"
+)
+
+// avoid import errors
+var _ = fmt.Errorf
+
+func MakeGRPCServer(ctx context.Context, endpoints endpoint.Endpoints) pb.UserServiceServer {
+	options := []grpctransport.ServerOption{}
+	return &grpcServer{
+
+		createuser: grpctransport.NewServer(
+			ctx,
+			endpoints.CreateUserEndpoint,
+			decodeCreateUserRequest,
+			encodeCreateUserResponse,
+			options...,
+		),
+
+		getuser: grpctransport.NewServer(
+			ctx,
+			endpoints.GetUserEndpoint,
+			decodeGetUserRequest,
+			encodeGetUserResponse,
+			options...,
+		),
+	}
+}
+
+type grpcServer struct {
+	createuser grpctransport.Handler
+
+	getuser grpctransport.Handler
+}
+
+func (s *grpcServer) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	_, rep, err := s.createuser.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return rep.(*pb.CreateUserResponse), nil
+}
+
+func decodeCreateUserRequest(ctx context.Context, grpcReq interface{}) (interface{}, error) {
+	return grpcReq, nil
+}
+
+func encodeCreateUserResponse(ctx context.Context, response interface{}) (interface{}, error) {
+	resp := response.(*pb.CreateUserResponse)
+	return resp, nil
+}
+
+func (s *grpcServer) GetUser(ctx context.Context, req *pb.GetUserRequest) (*pb.GetUserResponse, error) {
+	_, rep, err := s.getuser.ServeGRPC(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return rep.(*pb.GetUserResponse), nil
+}
+
+func decodeGetUserRequest(ctx context.Context, grpcReq interface{}) (interface{}, error) {
+	return grpcReq, nil
+}
+
+func encodeGetUserResponse(ctx context.Context, response interface{}) (interface{}, error) {
+	resp := response.(*pb.GetUserResponse)
+	return resp, nil
+}
