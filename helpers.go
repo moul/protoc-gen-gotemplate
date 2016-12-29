22d4ea9a4427fdab7aa5565fac8a6641d2194c0d
Valerio Gheri <valerio.gheri@gmail.com>
2016-12-29 17:21:53 +0100

    Close #34

--- helpers.go
+++ helpers.go
@@ -85,8 +85,7 @@ func getMessageType(f *descriptor.FileDescriptorProto, name string) *descriptor.
 }
 
 func isFieldMessage(f *descriptor.FieldDescriptorProto) bool {
-	if f.Type != nil && *f.Type == descriptor.FieldDescriptorProto_TYPE_MESSAGE &&
-		f.Label != nil && *f.Label != descriptor.FieldDescriptorProto_LABEL_REPEATED {
+	if f.Type != nil && *f.Type == descriptor.FieldDescriptorProto_TYPE_MESSAGE {
 		return true
 	}
 
